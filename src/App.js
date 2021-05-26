3d8f87d1b7885a165fd20c7fba5e84a75419887c
tobo3199 <tobias.sauter.003@outlook.com>
2021-05-26 09:53:05 +0200

    base structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Container, Nav, Row, Col, Card, CardDeck, Carousel, Form, Button, Image, Alert, Modal } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container>
+
+
+        <Switch>
+          <Route exact path="/">
+            <Startseite/>
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
   );
 }
 
 export default App;
+
+function Startseite()  {
+  return(
+      <p>Startseite</p>
+  );
+}
