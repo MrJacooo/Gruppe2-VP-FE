e818af8107c76685d60ba10d2f25173059ec74a4
Storz Sebastian <sebastian.storz2003@gmail.com>
2021-05-26 11:22:03 +0200

    POST and DELETE

--- src/controller.js
+++ src/controller.js
@@ -1,7 +1,27 @@
 import React from "react";
 
-export async function getItems(fn) {
+export function getItems(fn) {
     fetch("http://localhost:8080/items")
         .then(r => r.json)
         .then(data => { fn(data) })
+}
+
+export function postItem(fn, item) {
+    fetch("http://localhost:8080/items", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Accept": "application/json",
+        },
+        mode: 'cors',
+        body: JSON.stringify(item)
+    })
+        .then(r = r.json).then(data => fn(data))
+}
+
+
+export function deleteItem(fn, id) {
+    fetch("http://localhost:8080/items/" + id, { method: 'delete' })
+        .then(r => r.json).then(data => fn(data))
 }
