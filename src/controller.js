707d9a215404fa7447723793bd57dad20de621fe
Storz Sebastian <sebastian.storz2003@gmail.com>
2021-06-02 10:43:22 +0200

    Bugfix: getItems

--- src/controller.js
+++ src/controller.js
@@ -2,23 +2,22 @@ import React from "react";
 
 export function getItems(fn) {
     fetch("http://localhost:8080/items")
-        .then(r => r.json)
+        .then(r => r.json())
         .then(data => { fn(data) })
 }
 
-export function postItem(fn, item) {
-    fetch("http://localhost:8080/items", {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-            "Accept": "application/json",
-        },
-        mode: 'cors',
-        body: JSON.stringify(item)
-    })
-        .then(r = r.json).then(data => fn(data))
-}
+// export function postItem(fn, item) {
+//     fetch("http://localhost:8080/items", {
+//         method: 'POST',
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Access-Control-Allow-Origin": "*",
+//             "Accept": "application/json",
+//         },
+//         mode: 'cors',
+//         body: JSON.stringify(item)
+//     }).then(r => r.json).then(data => fn(data))
+// }
 
 
 export function deleteItem(fn, id) {
